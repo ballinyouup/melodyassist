8d8b0e3c159ab0f2f734754f502c3437874dddf2
ballinyouup <bryanhuici@gmail.com>
2023-03-05 11:23:00 -0500

    init

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -14,6 +14,7 @@ const config = {
   parser: "@typescript-eslint/parser",
   parserOptions: {
     project: "./tsconfig.json",
+    tsconfigRootDir: __dirname,
   },
   plugins: ["@typescript-eslint"],
   extends: ["next/core-web-vitals", "plugin:@typescript-eslint/recommended"],
