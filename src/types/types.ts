250a83485cfc51ddd7d85439402dab455e1b20a9
ballinyouup <bryanhuici@gmail.com>
2023-03-09 14:09:26 -0500

    changed file name

--- src/types/types.ts
+++
@@ -1,5 +0,0 @@
-// import type { inferRouterOutputs } from "@trpc/server";
-// import {z} from "zod";
-// import type { AppRouter } from "~/server/api/root";
-
-// type RouterOutputs = inferRouterOutputs<AppRouter>
