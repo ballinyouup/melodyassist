3554b354f6dd4274bd159b7492e87cce97162934
ballinyouup <bryanhuici@gmail.com>
2023-04-01 02:34:07 -0400

    removed volume from mobile in generate page

--- src/pages/components/Browse/BrowseFeed.tsx
+++ src/pages/components/Browse/BrowseFeed.tsx
@@ -38,40 +38,40 @@ const BrowseFeed: React.FC = () => {
           </div>
         </div>
       </div>
-        <div className="flex h-20 w-full flex-row items-center justify-center gap-4">
-          <button onClick={() => setVolume(volume === 0 ? 70 : 0)}>
-            {volume === 0 ? (
-              <Image
-                src="/volume-mute.png"
-                alt="volume muted"
-                className="w-7 invert"
-                width={28}
-                height={28}
-              />
-            ) : (
-              <Image
-                src="/audio.png"
-                alt="audio button"
-                className="w-8 invert"
-                width={32}
-                height={32}
-              />
-            )}
-          </button>
-          <div
-            className="tooltip tooltip-bottom tooltip-primary w-4/5 max-w-md sm:w-full"
-            data-tip={volume}
-          >
-            <input
-              type="range"
-              min="0"
-              max="100"
-              value={volume}
-              className="range range-xs w-full"
-              onChange={handleVolumeChange}
+      <div className="hidden h-20 w-full flex-row items-center justify-center gap-4 sm:flex">
+        <button onClick={() => setVolume(volume === 0 ? 70 : 0)}>
+          {volume === 0 ? (
+            <Image
+              src="/volume-mute.png"
+              alt="volume muted"
+              className="w-7 invert"
+              width={28}
+              height={28}
             />
-          </div>
+          ) : (
+            <Image
+              src="/audio.png"
+              alt="audio button"
+              className="w-8 invert"
+              width={32}
+              height={32}
+            />
+          )}
+        </button>
+        <div
+          className="tooltip tooltip-bottom tooltip-primary w-4/5 max-w-md sm:w-full"
+          data-tip={volume}
+        >
+          <input
+            type="range"
+            min="0"
+            max="100"
+            value={volume}
+            className="range range-xs w-full"
+            onChange={handleVolumeChange}
+          />
         </div>
+      </div>
       <div className="flex w-full flex-row items-start justify-center">
         <div className="relative h-[520px] w-full max-w-2xl gap-1 overflow-y-auto rounded-xl bg-base-300 p-1">
           {!feedLoading ? (
