6bf7662aade2ffa7cc372776c5d2edd37df9516f
ballinyouup <bryanhuici@gmail.com>
2023-04-05 16:30:51 -0400

    removed padding for mobile

--- src/pages/components/Home/Stats.tsx
+++ src/pages/components/Home/Stats.tsx
@@ -5,7 +5,7 @@ const Stats = () => {
   const { data: users, isLoading: userCountLoading } =
     api.account.getUserCount.useQuery();
   return (
-    <div className="flex w-full flex-col items-center justify-center pb-8">
+    <div className="flex w-full flex-col items-center justify-center sm:pb-8">
       <div className="stats w-full grid-flow-row justify-center border border-gray-500 border-opacity-20 p-5 text-center sm:w-fit sm:grid-flow-col md:mt-2">
         <div className="stat border-none">
           <div className="stat-title font-bold text-black text-opacity-80">
