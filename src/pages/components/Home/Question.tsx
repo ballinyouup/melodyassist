c5e1c4638265eae7a607f23d07d59815ca57ae1f
ballinyouup <bryanhuici@gmail.com>
2023-04-05 17:07:07 -0400

    fixed some faq styling

--- src/pages/components/Home/Question.tsx
+++ src/pages/components/Home/Question.tsx
@@ -7,10 +7,10 @@ const Question: React.FC<IQuestion> = ({ question, answer }) => {
   return (
     <div className="collapse">
       <input type="checkbox" className="peer" />
-      <div className="collapse-title bg-primary text-primary-content peer-checked:bg-secondary peer-checked:text-secondary-content">
+      <div className="collapse-title bg-primary text-primary-content peer-checked:bg-primary peer-checked:text-secondary-content">
         {question}
       </div>
-      <span className="collapse-content bg-primary text-primary-content peer-checked:bg-secondary peer-checked:text-secondary-content">
+      <span className="collapse-content bg-primary-content text-primary-content peer-checked:bg-secondary peer-checked:p-4 peer-checked:text-secondary-content">
         <p>{answer}</p>
       </span>
     </div>
