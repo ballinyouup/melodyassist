51d4c5072411991d9bb4a426c38134a6b4e4c063
ballinyouup <bryanhuici@gmail.com>
2023-04-05 12:55:30 -0400

    added error handling try/catch to all routers

--- src/server/api/routers/account.ts
+++ src/server/api/routers/account.ts
@@ -1,4 +1,5 @@
 //import { z } from "zod";
+import { Prisma } from "@prisma/client";
 import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 import {
@@ -13,90 +14,180 @@ export const accountRouter = createTRPCRouter({
   // 1. Retrieves the email address associated with the session.
   // 2. Deletes the user account with the retrieved email address using the Prisma delete method.
   deleteAccount: protectedProcedure.mutation(async ({ ctx }) => {
-    const existingUser = await ctx.prisma.user.findFirst({
-      where: { id: ctx.session.user.id },
-    });
-    if (existingUser) {
-      return await ctx.prisma.user.delete({
-        where: { id: existingUser.id },
+    try {
+      const existingUser = await ctx.prisma.user.findFirst({
+        where: { id: ctx.session.user.id },
       });
+      if (existingUser) {
+        return await ctx.prisma.user.delete({
+          where: { id: existingUser.id },
+        });
+      }
+    } catch (error) {
+      if (error instanceof TRPCError) {
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+          message: `Error Deleting Account. Error: ${error.message}`,
+        });
+      } else if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+          message: `Error from database while deleting account. Error: ${error.message}`,
+        });
+      } else {
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "Something went wrong while deleting account.",
+        });
+      }
     }
-    throw new Error("Error Deleting Account");
   }),
 
   getUserData: protectedProcedure.query(async ({ ctx }) => {
-    const existingUser = await ctx.prisma.user.findFirst({
-      where: { id: ctx.session.user.id },
-    });
-
-    if (existingUser) {
-      return ctx.prisma.user.findUnique({
-        where: { id: existingUser.id },
+    try {
+      const existingUser = await ctx.prisma.user.findFirst({
+        where: { id: ctx.session.user.id },
       });
-    }
 
-    throw new Error("User doesn't exist");
+      if (existingUser) {
+        return ctx.prisma.user.findUnique({
+          where: { id: existingUser.id },
+        });
+      }
+    } catch (error) {
+      if (error instanceof TRPCError) {
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+          message: `Error Getting User Data. Error: ${error.message}`,
+        });
+      } else if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+          message: `Error from database while getting user data. Error: ${error.message}`,
+        });
+      } else {
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "Something went wrong while getting user data.",
+        });
+      }
+    }
   }),
 
   updateTheme: protectedProcedure.mutation(async ({ ctx }) => {
-    // Check if user exists
-    const existingUser = await ctx.prisma.user.findFirst({
-      where: { id: ctx.session.user.id },
-    });
-
-    if (existingUser) {
-      return ctx.prisma.user.update({
-        where: { id: existingUser.id },
-        data: { theme: existingUser.theme === "winter" ? "night" : "winter" },
+    try {
+      // Check if user exists
+      const existingUser = await ctx.prisma.user.findFirst({
+        where: { id: ctx.session.user.id },
       });
-    }
 
-    throw new Error("Error Changing Theme");
+      if (existingUser) {
+        return ctx.prisma.user.update({
+          where: { id: existingUser.id },
+          data: { theme: existingUser.theme === "winter" ? "night" : "winter" },
+        });
+      }
+    } catch (error) {
+      if (error instanceof TRPCError) {
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+          message: `Error Updating Theme. Error: ${error.message}`,
+        });
+      } else if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+          message: `Error from database while updating theme. Error: ${error.message}`,
+        });
+      } else {
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "Something went wrong while updating theme.",
+        });
+      }
+    }
   }),
   getUserCount: publicProcedure.query(async ({ ctx }) => {
-    const users = await ctx.prisma.user.findMany();
-    if (users) {
-      return users;
+    try {
+      const users = await ctx.prisma.user.findMany();
+      if (users) {
+        return users;
+      }
+      throw new TRPCError({
+        code: "BAD_REQUEST",
+        message: "Error Fetching Feed",
+      });
+    } catch (error) {
+      if (error instanceof TRPCError) {
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+          message: `Error Getting User count. Error: ${error.message}`,
+        });
+      } else if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+          message: `Error from database while getting user count. Error: ${error.message}`,
+        });
+      } else {
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "Something went wrong while getting user count.",
+        });
+      }
     }
-    throw new TRPCError({
-      code: "BAD_REQUEST",
-      message: "Error Fetching Feed",
-    });
   }),
 
   updateUsername: protectedProcedure
     .input(z.string())
     .mutation(async ({ ctx, input }) => {
-      // Check if the new username is already taken
-      const currentUser = await ctx.prisma.user.findFirst({
-        where: { id: ctx.session.user.id },
-      });
-
-      const existingUsername = await ctx.prisma.user.findFirst({
-        where: { userName: input },
-      });
-
-      if (!currentUser) {
-        throw new TRPCError({
-          code: "UNAUTHORIZED",
-          message: "Not Authorized to perform this action",
+      try {
+        // Check if the new username is already taken
+        const currentUser = await ctx.prisma.user.findFirst({
+          where: { id: ctx.session.user.id },
         });
-      }
 
-      if (existingUsername) {
-        throw new TRPCError({
-          code: "BAD_REQUEST",
-          message: "Username already taken",
+        const existingUsername = await ctx.prisma.user.findFirst({
+          where: { userName: input },
         });
-      } else {
-        return await ctx.prisma.user.update({
-          where: {
-            id: currentUser.id,
-          },
-          data: {
-            userName: input,
-          },
-        });
+
+        if (!currentUser) {
+          throw new TRPCError({
+            code: "UNAUTHORIZED",
+            message: "Not Authorized to perform this action",
+          });
+        }
+
+        if (existingUsername) {
+          throw new TRPCError({
+            code: "BAD_REQUEST",
+            message: "Username already taken",
+          });
+        } else {
+          return await ctx.prisma.user.update({
+            where: {
+              id: currentUser.id,
+            },
+            data: {
+              userName: input,
+            },
+          });
+        }
+      } catch (error) {
+        if (error instanceof TRPCError) {
+          throw new TRPCError({
+            code: "BAD_REQUEST",
+            message: `Error Updating Username. Error: ${error.message}`,
+          });
+        } else if (error instanceof Prisma.PrismaClientKnownRequestError) {
+          throw new TRPCError({
+            code: "BAD_REQUEST",
+            message: `Error from database while updating username. Error: ${error.message}`,
+          });
+        } else {
+          throw new TRPCError({
+            code: "INTERNAL_SERVER_ERROR",
+            message: "Something went wrong while updating username.",
+          });
+        }
       }
     }),
 });
--- src/server/api/routers/audio.ts
+++ src/server/api/routers/audio.ts
@@ -6,6 +6,7 @@ import {
 } from "~/server/api/trpc";
 import { env } from "~/env.mjs";
 import { TRPCError } from "@trpc/server";
+import { Prisma } from "@prisma/client";
 
 const predictionSchema = z.object({
   completed_at: z.string().nullable(),
@@ -60,109 +61,40 @@ const PredictionCompletedWithPrediction = PredictionCompleted.extend({
 
 export const audioRouter = createTRPCRouter({
   getPrediction: protectedProcedure.query(async ({ ctx }) => {
-    const existingUser = await ctx.prisma.user.findUnique({
-      where: { id: ctx.session.user.id },
-    });
-
-    if (existingUser) {
-      const response = await fetch("https://api.replicate.com/v1/predictions", {
-        method: "POST",
-        headers: {
-          Authorization: `Token ${env.REPLICATE_API_TOKEN}`,
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          version:
-            "0de4a5f14b9120ce02c590eb9cf6c94841569fafbc4be7ab37436ce738bcf49f",
-          input: { seed: "-1" },
-        }),
-      });
-
-      if (response.status !== 201) {
-        const error = await response.json();
-        throw new TRPCError({
-          code: "BAD_REQUEST",
-          message: `Error Uploading File. ${JSON.stringify(error)}`,
-        });
-      }
-
-      const prediction = (await response.json()) as Prediction | ErrorResponse;
-      return prediction;
-    }
-    if (!existingUser) {
-      throw new TRPCError({
-        code: "UNAUTHORIZED",
-        message: "Not Authorized to perform this action",
-      });
-    }
-
-    throw new TRPCError({
-      code: "BAD_REQUEST",
-      message: "Error Sending First Prediction",
-    });
-  }),
-
-  getPredictionData: protectedProcedure
-    .input(z.string())
-    .query(async ({ ctx, input }) => {
+    try {
       const existingUser = await ctx.prisma.user.findUnique({
         where: { id: ctx.session.user.id },
       });
+
       if (existingUser) {
-        const secondResponse = await fetch(
-          `https://api.replicate.com/v1/predictions/${input}`,
+        const response = await fetch(
+          "https://api.replicate.com/v1/predictions",
           {
+            method: "POST",
             headers: {
               Authorization: `Token ${env.REPLICATE_API_TOKEN}`,
               "Content-Type": "application/json",
             },
+            body: JSON.stringify({
+              version:
+                "0de4a5f14b9120ce02c590eb9cf6c94841569fafbc4be7ab37436ce738bcf49f",
+              input: { seed: "-1" },
+            }),
           }
         );
 
-        if (secondResponse.status !== 200) {
-          const error = await secondResponse.json();
+        if (response.status !== 201) {
+          const error = await response.json();
           throw new TRPCError({
             code: "BAD_REQUEST",
-            message: `Error Fetching AI Audio File. ${JSON.stringify(error)}`,
+            message: `Error Uploading File. ${JSON.stringify(error)}`,
           });
         }
 
-        const result = (await secondResponse.json()) as PredictionCompleted;
-
-        if (result.status === "succeeded") {
-          // Fetch the audio data from the URL
-          const audioResponse = await fetch(result.output);
-          const blob = await audioResponse.blob();
-
-          //Upload the file to the specified URL
-          const response = await fetch(
-            `https://api.upload.io/v2/accounts/W142hjQ/uploads/binary?fileName=Seed-${
-              result.logs?.split(" ")[2]?.split("ffmpeg")[0] as string
-            }.mp3`,
-            {
-              method: "POST",
-              headers: {
-                Authorization: `Bearer ${env.UPLOAD_API_KEY}`,
-                "Content-Type": "audio/mpeg", // Replace with the appropriate MIME type
-              },
-              body: blob,
-            }
-          );
-
-          if (response.status !== 200) {
-            const error = await response.json();
-            throw new TRPCError({
-              code: "BAD_REQUEST",
-              message: `Error Uploading File. ${JSON.stringify(error)}`,
-            });
-          }
-
-          const uploadResult = (await response.json()) as UploadResponse;
-          return uploadResult;
-          //return response;
-        } else {
-          return result;
-        }
+        const prediction = (await response.json()) as
+          | Prediction
+          | ErrorResponse;
+        return prediction;
       }
       if (!existingUser) {
         throw new TRPCError({
@@ -170,11 +102,113 @@ export const audioRouter = createTRPCRouter({
           message: "Not Authorized to perform this action",
         });
       }
+    } catch (error) {
+      if (error instanceof TRPCError) {
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+          message: `Error Creating Prediction. Error: ${error.message}`,
+        });
+      } else if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+          message: `Error from database while creating prediction. Error: ${error.message}`,
+        });
+      } else {
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "Something went wrong while creating prediction.",
+        });
+      }
+    }
+  }),
 
-      throw new TRPCError({
-        code: "BAD_REQUEST",
-        message: "Error Fetching Second Prediction",
-      });
+  getPredictionData: protectedProcedure
+    .input(z.string())
+    .query(async ({ ctx, input }) => {
+      try {
+        const existingUser = await ctx.prisma.user.findUnique({
+          where: { id: ctx.session.user.id },
+        });
+        if (existingUser) {
+          const secondResponse = await fetch(
+            `https://api.replicate.com/v1/predictions/${input}`,
+            {
+              headers: {
+                Authorization: `Token ${env.REPLICATE_API_TOKEN}`,
+                "Content-Type": "application/json",
+              },
+            }
+          );
+
+          if (secondResponse.status !== 200) {
+            const error = await secondResponse.json();
+            throw new TRPCError({
+              code: "BAD_REQUEST",
+              message: `Error Fetching AI Audio File. ${JSON.stringify(error)}`,
+            });
+          }
+
+          const result = (await secondResponse.json()) as PredictionCompleted;
+
+          if (result.status === "succeeded") {
+            // Fetch the audio data from the URL
+            const audioResponse = await fetch(result.output);
+            const blob = await audioResponse.blob();
+
+            //Upload the file to the specified URL
+            const response = await fetch(
+              `https://api.upload.io/v2/accounts/W142hjQ/uploads/binary?fileName=Seed-${
+                result.logs?.split(" ")[2]?.split("ffmpeg")[0] as string
+              }.mp3`,
+              {
+                method: "POST",
+                headers: {
+                  Authorization: `Bearer ${env.UPLOAD_API_KEY}`,
+                  "Content-Type": "audio/mpeg", // Replace with the appropriate MIME type
+                },
+                body: blob,
+              }
+            );
+
+            if (response.status !== 200) {
+              const error = await response.json();
+              throw new TRPCError({
+                code: "BAD_REQUEST",
+                message: `Error Uploading File. ${JSON.stringify(error)}`,
+              });
+            }
+
+            const uploadResult = (await response.json()) as UploadResponse;
+            return uploadResult;
+            //return response;
+          } else {
+            return result;
+          }
+        }
+        if (!existingUser) {
+          throw new TRPCError({
+            code: "UNAUTHORIZED",
+            message: "Not Authorized to perform this action",
+          });
+        }
+      } catch (error) {
+        if (error instanceof TRPCError) {
+          throw new TRPCError({
+            code: "BAD_REQUEST",
+            message: `Error Getting Second Prediction. Error: ${error.message}`,
+          });
+        } else if (error instanceof Prisma.PrismaClientKnownRequestError) {
+          throw new TRPCError({
+            code: "BAD_REQUEST",
+            message: `Error from database while getting second prediction. Error: ${error.message}`,
+          });
+        } else {
+          throw new TRPCError({
+            code: "INTERNAL_SERVER_ERROR",
+            message: "Something went wrong while getting second prediction.",
+          });
+        }
+      }
     }),
   createAudio: protectedProcedure
     .input(
@@ -184,23 +218,77 @@ export const audioRouter = createTRPCRouter({
       })
     )
     .mutation(async ({ ctx, input }) => {
+      try {
+        // Check if user exists
+        const existingUser = await ctx.prisma.user.findFirst({
+          where: { id: ctx.session.user.id },
+        });
+
+        if (existingUser) {
+          return ctx.prisma.user.update({
+            where: { id: existingUser.id },
+            data: {
+              posts: {
+                create: {
+                  title: input.title,
+                  content: input.content,
+                },
+              },
+            },
+          });
+        }
+        if (!existingUser) {
+          throw new TRPCError({
+            code: "UNAUTHORIZED",
+            message: "Not Authorized to perform this action",
+          });
+        }
+      } catch (error) {
+        if (error instanceof TRPCError) {
+          throw new TRPCError({
+            code: "BAD_REQUEST",
+            message: `Error Creating Audio. Error: ${error.message}`,
+          });
+        } else if (error instanceof Prisma.PrismaClientKnownRequestError) {
+          throw new TRPCError({
+            code: "BAD_REQUEST",
+            message: `Error from database while creating audio. Error: ${error.message}`,
+          });
+        } else {
+          throw new TRPCError({
+            code: "INTERNAL_SERVER_ERROR",
+            message: "Something went wrong while creating audio.",
+          });
+        }
+      }
+    }),
+  getAudio: protectedProcedure.query(async ({ ctx }) => {
+    try {
       // Check if user exists
       const existingUser = await ctx.prisma.user.findFirst({
         where: { id: ctx.session.user.id },
       });
-
       if (existingUser) {
-        return ctx.prisma.user.update({
-          where: { id: existingUser.id },
-          data: {
+        const userAudios = await ctx.prisma.user.findUnique({
+          where: {
+            id: existingUser.id,
+          },
+          select: {
             posts: {
-              create: {
-                title: input.title,
-                content: input.content,
+              select: {
+                title: true,
+                content: true,
+                createdAt: true,
+                id: true,
+                author: true,
+              },
+              orderBy: {
+                createdAt: "desc",
               },
             },
           },
         });
+        return userAudios;
       }
       if (!existingUser) {
         throw new TRPCError({
@@ -208,102 +296,70 @@ export const audioRouter = createTRPCRouter({
           message: "Not Authorized to perform this action",
         });
       }
-      throw new TRPCError({
-        code: "BAD_REQUEST",
-        message: "Error Creating Audio",
-      });
-    }),
-  getAudio: protectedProcedure.query(async ({ ctx }) => {
-    // Check if user exists
-    const existingUser = await ctx.prisma.user.findFirst({
-      where: { id: ctx.session.user.id },
-    });
-    if (existingUser) {
-      const userAudios = await ctx.prisma.user.findUnique({
-        where: {
-          id: existingUser.id,
-        },
-        select: {
-          posts: {
-            select: {
-              title: true,
-              content: true,
-              createdAt: true,
-              id: true,
-              author: true,
-            },
-            orderBy: {
-              createdAt: "desc",
-            },
-          },
-        },
-      });
-      return userAudios;
+    } catch (error) {
+      if (error instanceof TRPCError) {
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+          message: `Error Fetching User Audio. Error: ${error.message}`,
+        });
+      } else if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+          message: `Error from database while getting user audio. Error: ${error.message}`,
+        });
+      } else {
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "Something went wrong while fetching for user audio.",
+        });
+      }
     }
-    if (!existingUser) {
-      throw new TRPCError({
-        code: "UNAUTHORIZED",
-        message: "Not Authorized to perform this action",
-      });
-    }
-    throw new TRPCError({
-      code: "BAD_REQUEST",
-      message: "Error Fetching All User Audio",
-    });
   }),
   getFeed: publicProcedure.query(async ({ ctx }) => {
-    const feed = await ctx.prisma.post.findMany({
-      select: {
-        title: true,
-        content: true,
-        createdAt: true,
-        id: true,
-        author: true,
-      },
-      orderBy: {
-        createdAt: "desc",
-      },
-    });
-    if (feed) {
-      return feed;
-    }
-    throw new TRPCError({
-      code: "BAD_REQUEST",
-      message: "Error Fetching Feed",
-    });
-  }),
-  deleteAllAudio: protectedProcedure.mutation(async ({ ctx }) => {
-    const existingUser = await ctx.prisma.user.findFirst({
-      where: { id: ctx.session.user.id },
-    });
-    if (existingUser) {
-      return ctx.prisma.post.deleteMany({
-        where: {
-          authorId: existingUser.id,
+    try {
+      const feed = await ctx.prisma.post.findMany({
+        select: {
+          title: true,
+          content: true,
+          createdAt: true,
+          id: true,
+          author: true,
+        },
+        orderBy: {
+          createdAt: "desc",
         },
       });
+      if (feed) {
+        return feed;
+      }
+    } catch (error) {
+      if (error instanceof TRPCError) {
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+          message: `Error Getting Feed. Error: ${error.message}`,
+        });
+      } else if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+          message: `Error from database while getting feed. Error: ${error.message}`,
+        });
+      } else {
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "Something went wrong while getting feed.",
+        });
+      }
     }
-    if (!existingUser) {
-      throw new TRPCError({
-        code: "UNAUTHORIZED",
-        message: "Not Authorized to perform this action",
-      });
-    }
-    throw new TRPCError({
-      code: "BAD_REQUEST",
-      message: "Error Deleting All User Audio",
-    });
   }),
-  deleteAudio: protectedProcedure
-    .input(z.string())
-    .mutation(async ({ ctx, input }) => {
+  deleteAllAudio: protectedProcedure.mutation(async ({ ctx }) => {
+    try {
       const existingUser = await ctx.prisma.user.findFirst({
         where: { id: ctx.session.user.id },
       });
       if (existingUser) {
-        return ctx.prisma.post.delete({
+        return ctx.prisma.post.deleteMany({
           where: {
-            id: input,
+            authorId: existingUser.id,
           },
         });
       }
@@ -313,9 +369,62 @@ export const audioRouter = createTRPCRouter({
           message: "Not Authorized to perform this action",
         });
       }
-      throw new TRPCError({
-        code: "BAD_REQUEST",
-        message: "Error Deleting Single Audio",
-      });
+    } catch (error) {
+      if (error instanceof TRPCError) {
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+          message: `Error Deleting Audio. Error: ${error.message}`,
+        });
+      } else if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+          message: `Error from database while deleting audio. Error: ${error.message}`,
+        });
+      } else {
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "Something went wrong while deleting all audio.",
+        });
+      }
+    }
+  }),
+  deleteAudio: protectedProcedure
+    .input(z.string())
+    .mutation(async ({ ctx, input }) => {
+      try {
+        const existingUser = await ctx.prisma.user.findFirst({
+          where: { id: ctx.session.user.id },
+        });
+        if (existingUser) {
+          return ctx.prisma.post.delete({
+            where: {
+              id: input,
+            },
+          });
+        }
+        if (!existingUser) {
+          throw new TRPCError({
+            code: "UNAUTHORIZED",
+            message: "Not Authorized to perform this action",
+          });
+        }
+      } catch (error) {
+        if (error instanceof TRPCError) {
+          throw new TRPCError({
+            code: "BAD_REQUEST",
+            message: `Error Deleting Audio. Error: ${error.message}`,
+          });
+        } else if (error instanceof Prisma.PrismaClientKnownRequestError) {
+          throw new TRPCError({
+            code: "BAD_REQUEST",
+            message: `Error from database while deleting audio. Error: ${error.message}`,
+          });
+        } else {
+          throw new TRPCError({
+            code: "INTERNAL_SERVER_ERROR",
+            message: "Something went wrong while deleting audio.",
+          });
+        }
+      }
     }),
 });
