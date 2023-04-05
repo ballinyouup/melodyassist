8ec46349935f857a9eb2c5aa5e40c608aba37488
ballinyouup <bryanhuici@gmail.com>
2023-04-05 17:00:11 -0400

    fixed divider styling

--- src/pages/components/Home/Divider.tsx
+++ src/pages/components/Home/Divider.tsx
@@ -1,7 +1,7 @@
 const Divider = () => {
   return (
     <div className="flex flex-row justify-center">
-      <div className="divider w-full max-w-5xl before:bg-transparent after:bg-transparent" />
+      <div className="divider h-8 m-4 w-full max-w-sm before:h-[0.05rem] before:bg-black before:bg-opacity-25 after:h-[0.05rem] after:bg-black after:bg-opacity-25 md:max-w-xl" />
     </div>
   );
 };
