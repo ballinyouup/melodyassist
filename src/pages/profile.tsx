bea3c2540a6205a17c2d4bae0e5072f163e8cf6f
ballinyouup <bryanhuici@gmail.com>
2023-04-07 11:16:04 -0400

    added a coming soon to profile page

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,7 +1,14 @@
 import Layout from "./Layout";
 
 function Profile() {
-  return <div className="h-screen">Profile</div>;
+  return (
+    <div className="relative h-screen">
+      <span className="absolute top-1/3 left-1/2 flex w-full -translate-x-1/2 -translate-y-1/2 flex-col items-center justify-center bg-black p-20 text-3xl font-medium text-white md:w-1/2">
+        <p>🛠️ Profile Page</p>
+        <p>Coming Soon 🛠️</p>
+      </span>
+    </div>
+  );
 }
 
 export default Layout(Profile);
